2cfcad6fc8d25f889de58906786533cba4ecb9af
adenexplore <112192244+adenexplore@users.noreply.github.com>
2023-11-11 12:48:01 +0700

    Create identitas.java
    first name

---
+++ identitas.java
@@ -0,0 +1,14 @@
+public class Identitas {
+    public static void main(String[] args) {
+        // Deklarasi variabel identitas
+        String nama = "Siti Jahriatussania";
+        int umur = 19;
+        float tinggi = 148.5f;
+
+        // Menampilkan identitas
+        System.out.println("Identitas:");
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Tinggi: " + tinggi + " cm");
+    }
+}
